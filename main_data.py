a585d33d2d706c107dc18ef97657636170ce949c
WangLuran <94464527+WangLuran@users.noreply.github.com>
2024-09-24 19:40:49 +0100

    Update main_data.py

--- main_data.py
+++ main_data.py
@@ -15,22 +15,50 @@ import torch
 from torch.utils.data import Dataset
 import torch.nn.functional as F
 from torchvision import models
+import numpy as np
+from torchvision import datasets, transforms
+import imageio
+import json
+
+FLAGS = flags.FLAGS
+
+config_flags.DEFINE_config_file("config", None, "Rectified Flow Model configuration.", lock_config=True)
+flags.DEFINE_integer("batch_size", 1, "batch size")
+flags.DEFINE_integer("index", 0, "position of samples")
+text_prompts = ['A photo of an old face.','A photo of a sad face.','A photo of a smiling face.','A photo of an angry face.','A photo of a face with curly hair.']
+alpha = 0.7
+model_path = '../checkpoint_10.pth'
+
+# # Create an empty dictionary
+# empty_dict = {}
+
+# # Define the path for the new empty JSON file
+# file_path = 'output/results.json'
+
+# # Write the empty dictionary to the file
+# with open(file_path, 'w') as json_file:
+#     json.dump(empty_dict, json_file)
+
+# print(f"Empty JSON file created at {file_path}")
+
+def get_img(path=None):
+    img = imageio.imread(path) ### 4-no expression
+    print(img.shape)
+    img = img / 255.
+    img = img[np.newaxis, :, :, :]
+    img = img.transpose(0, 3, 1, 2)
+    print('read image from:', path, 'img range:', img.min(), img.max())
+    img = torch.tensor(img).float()
+    img = torch.nn.functional.interpolate(img, size=256)
+
+    return img
 
 # Path to your CelebA images folder
-image_dir = 'path_to_your_downloaded_celeba_images'
-
-# Transform to resize and convert to tensor
-transform = transforms.Compose([
-    transforms.Resize((128, 128)),  # Resize images to 128x128 pixels
-    transforms.ToTensor(),          # Convert to tensor
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize
-])
-
+image_dir = 'data_celeba_hq_1024'
 
 class CelebADataset(Dataset):
-    def __init__(self, img_dir, transform=None):
+    def __init__(self, img_dir):
         self.img_dir = img_dir
-        self.transform = transform
         self.image_files = os.listdir(img_dir)  # Get list of image filenames
 
     def __len__(self):
@@ -38,40 +66,36 @@ class CelebADataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.image_files[idx])
-        image = Image.open(img_path).convert("RGB")  # Load image
-        
-        if self.transform:
-            image = self.transform(image)
+        image = get_img(img_path)  # Load image
         
         return image
 
 # Create the dataset and DataLoader
-celeba_dataset = CelebADataset(image_dir, transform=transform)
+celeba_dataset = CelebADataset(image_dir)
 
 print('data finish')
 
 # Set random seed for reproducibility
 np.random.seed(42)
 
-# Generate random indices for sampling 1,000 images
-indices = np.random.choice(len(celeb_a_dataset), 1000, replace=False)
-
-# Create a subset sampler
-sampler = SubsetRandomSampler(indices)
-
-# Load the sampled subset with a DataLoader
-data_loader = DataLoader(celeb_a_dataset, sampler=sampler, batch_size=64)
-
-print('data loader finish')
-
-config = RectifiedFlow/configs/celeba_hq_pytorch_rf_gaussian.py
-text_prompt = 'A photo of a smiling face.'
-alpha = 0.7
-model_path = '../checkpoint_10.pth'
 
 def main(argv):
-  run_lib_flowgrad_oc.flowgrad_edit(config, text_prompt, alpha, model_path, data_loader, output_folder)
+  # Generate random indices for sampling 1,000 images
+  indices = [i+FLAGS.index for i in range(100)]
+
+  # Create a subset sampler
+  sampler = SubsetRandomSampler(indices)
+  # Load the sampled subset with a DataLoader
+  data_loader = DataLoader(celeba_dataset, sampler=sampler, batch_size=FLAGS.batch_size)
+
+  print('data loader finish')
+
+  #c, l, i = run_lib_flowgrad_oc.flowgrad_edit(FLAGS.config, text_prompts, alpha, model_path, data_loader)
+  _ = run_lib_flowgrad_oc.dflow_edit(FLAGS.config, text_prompts, alpha, model_path, data_loader)
+
+
 
 
 if __name__ == "__main__":
   app.run(main)
+
